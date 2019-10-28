9b7a9545791c5d4a914957bb07451373b07082e8
scarley <stcarley@gmail.com>
2019-10-28 12:30:11 -0700

    Replaced hard coding with variables in some cases, changed default report to learner activity detail

--- learningActivityReports.py
+++ learningActivityReports.py
@@ -1,9 +1,27 @@
 from oAuth import token
 import requests
+import json
+import time
+
+#converting current time from seconds since epoch to milliseconds since epoch
+currentTime = int(time.time() * 1000)
+print(currentTime)
+offsetDuration = 14
+#startedAt is milliseconds since epoch, adjusted based upon the offset duration
+startedAt = currentTime - (86400000 * offsetDuration)
 
 bearerToken = "Bearer "+token
 url = "https://api.linkedin.com/v2/learningActivityReports"
-querystring = {"q":"criteria","count":"1","startedAt":"1562699900247","timeOffset.unit":"DAY","timeOffset.duration":"7","aggregationCriteria.primary":"ACCOUNT"}
+
+#learner activity detail report
+querystring = {"q":"criteria",
+               "count":"100",
+               "startedAt":startedAt,
+               "timeOffset.unit":"DAY",
+               "timeOffset.duration":offsetDuration,
+               "aggregationCriteria.primary":"INDIVIDUAL",
+               "aggregationCriteria.secondary":"CONTENT",
+               "assetType":"COURSE"}
 headers = {
     'Authorization': bearerToken,
     'Accept': "*/*",
@@ -12,4 +30,6 @@ headers = {
     'cache-control': "no-cache"
 }
 response = requests.request("GET", url, headers=headers, params=querystring)
-print(response.text)
+jsonResponse = json.loads(response.text)
+print(json.dumps(jsonResponse, indent=4, sort_keys=True))
+#print(response.text)
