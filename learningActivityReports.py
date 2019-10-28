6704f784aac50658a99d510e131566685ed5ee2b
scarley <stcarley@gmail.com>
2019-10-28 14:59:45 -0700

    Comments

--- learningActivityReports.py
+++ learningActivityReports.py
@@ -10,7 +10,7 @@ currentTime = int(time.time() * 1000)
 # QUERY PARAMETERS #
 numberOfResults = 100
 offsetUnit = "DAY"
-offsetDuration = 14
+offsetDuration = 14  #maximum offset is 14 days, larger calls will return 500 error
 #startedAt is milliseconds since epoch, adjusted based upon the offset duration
 startedAt = currentTime - (86400000 * offsetDuration)  #86400000 is number of milliseconds in a day
 
@@ -30,6 +30,7 @@ querystring = {"q":"criteria",
                "sortBy.engagementMetricType":"COMPLETIONS",
                "assetType":"COURSE"}
 
+#if using CONTENT aggregation criteria you must include assetType COURSE
 
 headers = {
     'Authorization': bearerToken,
